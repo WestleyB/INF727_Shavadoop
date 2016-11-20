d13095128aca58328d4a65635f1ab37eba08f631
WestleyB <westley.birmingham@telecom-paristech.fr>
2016-11-20 15:11:38 +0100

    first maj

--- MASTER_SHAVADOOP_JAR/src/master_shavadoop/functions_tool.java
+++ MASTER_SHAVADOOP_JAR/src/master_shavadoop/functions_tool.java
@@ -9,217 +9,239 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class functions_tool {
 
-// 		1- Sx Splitting Function
-public static HashMap <String,String> splitting_function(ArrayList <String> ips_list_ok, String input_file, String login, String path_cible){
-	System.out.println("\nSplitting Function Launched");
-	
-	// Définition des dictionnaires de clés
-	HashMap <String,String> master_split_Sx = new HashMap <String, String>();
-	ArrayList <String> list_master_split = new ArrayList <String>();
-	HashMap <String,String> master_cle_UMx_tmp = new HashMap <String, String>();
-	
-	// chargement du fichier Input.txt
-	list_master_split = functions_tool.read_file(input_file);
-	
-	// création des différents fichiers Sx et du dictionnaire contenant leurs chemins
-	for(String el : list_master_split){
-		if(!master_split_Sx.containsKey(el)){
-			String file_key = "S" + list_master_split.indexOf(el);
-			String file_key_path = System.getProperty("user.dir") + "/" + file_key +".txt";
-			new File(file_key_path).delete();
-			functions_tool.write_file(file_key_path, el);
-			master_split_Sx.put(file_key, file_key_path);
-			
-			// 2 - Création Dictionnaire UMx - machines
-			int ip_cpt = 0;
-			//if(list_master_split.size() <= ips_list_ok.size()){
-				for(String ip_ok_from_list : ips_list_ok){
-					if(ip_cpt == list_master_split.indexOf(el)){
-						master_cle_UMx_tmp.put(ip_ok_from_list.split(" ")[1], file_key);
-						// Envoi du fchier Sx vers la machine distante
-						functions_tool.copy_file_to(ip_ok_from_list.split(" ")[1], login, file_key_path, path_cible + file_key);
-					}
-					ip_cpt += 1;
-			//	}		
+	public static void deleteOldFiles(String directory) {
+		File file = new File(directory);
+		String[] myFiles;
+		if (file.isDirectory()) {
+			myFiles = file.list();
+			for (int i = 0; i < myFiles.length; i++) {
+				File myFile = new File(file, myFiles[i]);
+				if (!myFiles[i].equals("slave_calcul.jar") && !myFiles[i].equals("motsIgnores.txt"))
+					myFile.delete();
 			}
 		}
 	}
-	// System.out.println(master_split_Sx);
-	// System.out.println(master_cle_UMx_tmp);
-	return master_cle_UMx_tmp;
-}
 
+	public static void simultation_function(HashMap<String, Double> ips_list_ok, String login, String path_cible, String file_cible,
+			String params) throws IOException, InterruptedException {
+		ArrayList<exec_parallele> threads = new ArrayList<exec_parallele>();
+		ArrayList<String> key_return_tmp = new ArrayList<String>();
 
-// Envoi du Fichier JAR par SCP vers la machine distante
-	public static void copy_file_to(String ip, String login, String rFile, String lFile){
-		try {
-				Process cpyFileLocal = Runtime.getRuntime().exec("scp  " + rFile + " " + login + "@" + ip + ":" + lFile);
-//				InputStream stderr = cpyFileLocal.getErrorStream();
-//				InputStreamReader isr = new InputStreamReader(stderr);
-//				BufferedReader br = new BufferedReader(isr);
-//				String line = null;
-//				int exitVal = cpyFileLocal.waitFor();
-			} catch (Exception ex) {
-				ex.printStackTrace();
+		System.out.println("Test d'execution " + params);
+
+		// Select the worker with the best response time
+		String ip_selected = ips_list_ok.keySet().toArray(new String[0])[0];
+
+		// Compute on one distant worker
+		functions_tool.compute_slave_distant(ip_selected, login, path_cible,file_cible, params);
+		System.out.println("Fin d'execution " + params);
+
+		// functions_tool.launch_parallel_exec(ips_list_ok, login, file_cible,
+		// params); // => Probleme de parallelisation
+
+		// Execution parallelisée de la simulation de calcul
+		params = " " + "simul";
+		for (String ip_ok_from_list : ips_list_ok.keySet()) {
+			exec_parallele thread_ip = new exec_parallele(ip_ok_from_list, login, path_cible, file_cible, params);
+			thread_ip.start();
+			threads.add(thread_ip);
+		}
+
+		// Attente de la fin d'execution de tous les threads
+		for (exec_parallele thread_ip_launched : threads) {
+			thread_ip_launched.join();
+			for (String log_return : thread_ip_launched.getLogs_return()) {
+				key_return_tmp.add(log_return);
 			}
+		}
+		System.out.println("Fin d'execution :" + params);
 	}
 
+	// Sx Splitting Function
+	public static HashMap<String, String> splitting_function(HashMap<String, Double> ips_list_ok, String login,
+			String input_file, String path_cible) {
+		// Définition des dictionnaires de clés
+		HashMap<String, String> master_split_Sx = new HashMap<String, String>();
+		ArrayList<String> list_to_split = new ArrayList<String>();
+		int index_select_ip = 0;
+		// chargement du fichier Input.txt
+		list_to_split = functions_tool.read_file(input_file);
 
-	//Stockage des IP dans le fichier de logs
-	public static void write_file(String file_name, String text){
-		//String file_path = "/Users/Wes/Documents/workspace/TPTelecom/MASTER_SHAVADOOP_JAR/" + file_name;
-	
-		try{
+		// création des différents fichiers Sx
+		for (String line_to_sx : list_to_split) {
+			if (!master_split_Sx.containsKey(line_to_sx) && !line_to_sx.isEmpty()) {
+				String file_key = "S" + list_to_split.indexOf(line_to_sx);
+				new File(file_key).delete();
+				line_to_sx = functions_tool.word_cleaner_regex(line_to_sx);
+				functions_tool.write_file(file_key, line_to_sx);
+				String ip_word_selected = ips_list_ok.keySet().toArray(new String[index_select_ip])[index_select_ip];
+				functions_tool.copy_file_to(ip_word_selected, login, file_key, path_cible, file_key);
+				master_split_Sx.put(file_key, ip_word_selected);
+				if (index_select_ip < ips_list_ok.keySet().size() - 1){
+					index_select_ip += 1;
+				} else {
+					index_select_ip = 0;
+				}
+			}
+		}
+		return master_split_Sx;
+	}
+
+	// Send .jar file to distant worker
+	public static void copy_file_to(String ip, String login, String rFile, String lpath, String lFile) {
+		try {
+			Runtime.getRuntime().exec("scp  " + rFile + " " + login + "@" + ip + ":" + lpath + lFile);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	// Save available worker's IP to a log file
+	public static void write_file(String file_name, String text) {
+		try {
 			FileWriter file_writer = new FileWriter(file_name, true);
 			BufferedWriter output = new BufferedWriter(file_writer);
-			output.write(text+"\n");
+			output.write(text + "\n");
 			output.flush();
 			output.close();
-			
-		}
-		catch(IOException ioe){
+
+		} catch (IOException ioe) {
 			ioe.printStackTrace();
-			
+
 		}
 	}
 
-
-	// lecture des IP stockées dans le fichier de logs
-	public static ArrayList <String> read_file(String file){
-		ArrayList <String> ips_list = new ArrayList <String>();
-		try{
-				// read text file
-				InputStream ips = new FileInputStream(file);
-				InputStreamReader ipsr = new InputStreamReader(ips);
-				BufferedReader buffered_reader = new BufferedReader(ipsr);
-				String line;
-				while((line = buffered_reader.readLine())!=null){
+	// Read file
+	public static ArrayList<String> read_file(String file) {
+		ArrayList<String> ips_list = new ArrayList<String>();
+		try {
+			// read text file
+			InputStream ips = new FileInputStream(file);
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			BufferedReader buffered_reader = new BufferedReader(ipsr);
+			String line = null;
+			while ((line = buffered_reader.readLine()) != null) {
+				if(!line.isEmpty()){
 					ips_list.add(line);
+					
 				}
-				buffered_reader.close();
-		}
-		catch(Exception e){
+			}
+			buffered_reader.close();
+		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
 		return ips_list;
 	}
 
-
-	public static String connection_distant(String login, String ip) throws InterruptedException {
-		ProcessBuilder proc =  new ProcessBuilder("ssh", login + "@"+ ip, " BatchMode yes; echo $((2+3))");
-		Process proc_1;
-		try {
-			proc_1 = proc.start();
-			proc_1.waitFor();
-			if(proc_1.exitValue() == 0)
-				return "Connexion " + ip + " : OK";
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	return null;
-	}
-
-
-	//Vérification des machines alive via leur IP
-	public static String check_save_goods_ip(String login, String ip_source_list) throws InterruptedException, IOException{
-		ArrayList <String> ips_list_all = new ArrayList <String>();
-		ips_list_all = read_file(ip_source_list);
-		String file_path = System.getProperty("user.dir") + "/" + "logs_connexions.txt";
-		new File(file_path).delete();
-		new File(file_path).createNewFile();
-		for(String ip_from_list : ips_list_all){
-			String ip_tmp = connection_distant(login, ip_from_list);
-			System.out.println(ip_tmp);	
-			if(ip_tmp != null)
-				write_file(file_path, ip_tmp);
-		}
-		return file_path;
-	}
-
-
-	public static ArrayList<String> compute_slave_distant(String ip, String login, String file_cible, String param) throws IOException{
+	
+	public static ArrayList<String> compute_slave_distant(String ip, String login, String path_cible, String file_cible, String param)
+			throws IOException {
 		ArrayList<String> logs_return_tmp = new ArrayList<String>();
-		String file_source = System.getProperty("user.dir") + "/" + "slave_calcul.jar";
-		// Chemin du JAR : "Users/Wes/Documents/workspace/TPTelecom/master_shavadoop/slave_calcul.jar"
-		
+		String file_source = System.getProperty("user.dir") + "/" + file_cible;
+
 		long time_begining = java.lang.System.currentTimeMillis();
-		
+
 		// Envoi du fchier JAR vers la machine distante
-		copy_file_to(ip, login, file_source, file_cible);
-		
+		copy_file_to(ip, login, file_source, path_cible, file_cible);
+
 		// Execution du fichier JAR distant
-		ProcessBuilder process =  new ProcessBuilder("ssh", login + "@" + ip, " cd ~/workspace/TPTelecom/INF727/;java -jar slave_calcul.jar" + param);
-		//System.out.println("cmd :"+process.command());
+		ProcessBuilder process = new ProcessBuilder("ssh", login + "@" + ip,
+				" cd " + path_cible + ";java -jar " + file_cible + " " + param);
+		// System.out.println("cmd :"+process.command());
 		Process process_1 = process.start();
-		InputStream input_stream_1 = process_1.getInputStream(); 
+		InputStream input_stream_1 = process_1.getInputStream();
 		BufferedReader buffered_reader = null;
-		buffered_reader =new BufferedReader(new InputStreamReader(input_stream_1)); 
+		buffered_reader = new BufferedReader(new InputStreamReader(input_stream_1));
 		String line = null;
-		while((line = buffered_reader.readLine()) != null){
+		while ((line = buffered_reader.readLine()) != null) {
 			System.out.println(line);
-			//master_compute.key_return_tmp.add(line);
+			// master_compute.key_return_tmp.add(line);
 			logs_return_tmp.add(line);
 		}
-		System.out.println("Slave computations finished on "+ip);
 		long time_end = java.lang.System.currentTimeMillis();
-		System.out.println("Temps d'execution du calcul : " + (time_end - time_begining)/1000.0);
+		System.out.println("Slave computations finished on " + ip + " Temps d'execution : ["
+				+ (time_end - time_begining) / 1000.0 + "s]");
 		return logs_return_tmp;
 	}
-	
-	
-	//	Fonction Execution parallelisée /!\ => Probleme de parallelisation
-	public static ArrayList<String> launch_parallel_exec(ArrayList <String> ips_list_ok, String login, String file_cible, String params) throws InterruptedException{
-		ArrayList <exec_parallele> threads = new ArrayList <exec_parallele>();
-		ArrayList<String> key_return_tmp = new ArrayList<String>();
-		
-		for(String ip_ok_from_list : ips_list_ok){
-			exec_parallele thread_ip =  new exec_parallele(ip_ok_from_list.split(" ")[1], login, file_cible, params);
-			thread_ip.start();
-			threads.add(thread_ip);
-		}
-		
-		//Attente de la fin d'execution de tous les threads
-		for(exec_parallele thread_ip_launched : threads){
-			thread_ip_launched.join();
-			for(String log_return : thread_ip_launched.getLogs_return_tmp()){
-				key_return_tmp.add(log_return);
-			}
-		}
-		System.out.println("Fin d'execution :" + params);
-		return key_return_tmp;
-	}
 
-
-	public static HashMap<String, Integer> nOccurrences(ArrayList<String> list_sentence) {
+	
+	public static HashMap<String, Integer> words_count(ArrayList<String> list_sentence) {
 		HashMap<String, Integer> count = new HashMap<>();
 		for (String sentence : list_sentence) {
-			for (String word : sentence.split(" ")) {
-				if (!count.containsKey(word)) {
-					count.put(word, 0);
-				}
+			String word = sentence.split(" ")[0];
+			if (!count.containsKey(word)) {
+				count.put(word, 1);
+			} else {
 				count.put(word, count.get(word) + 1);
 			}
 		}
 		return count;
 	}
 
-
-	public static String keyMaxValue(HashMap<String, Integer> count) {
-		String maxKey = "";
-		int maxValue = 0;
 	
-		for (String key : count.keySet()) {
-			if (count.get(key) > maxValue) {
-				maxValue = count.get(key);
-				maxKey = key;
-			}
+	public static boolean isInteger(String s) {
+		try {
+			Integer.parseInt(s);
+			return true;
+		} catch (NumberFormatException nfe) {
+			return false;
 		}
-		return maxKey;
 	}
 
+	
+	// Sort HashMap by values
+	public static LinkedHashMap<String, Double> sort_hashmap_by_values(HashMap<String, Double> passedMap) {
+		List<String> mapKeys = new ArrayList<>(passedMap.keySet());
+		List<Double> mapValues = new ArrayList<>(passedMap.values());
+		Collections.sort(mapValues);
+		Collections.sort(mapKeys);
+
+		LinkedHashMap<String, Double> sortedMap = new LinkedHashMap<>();
+
+		Iterator<Double> valueIt = mapValues.iterator();
+		while (valueIt.hasNext()) {
+			Double val = valueIt.next();
+			Iterator<String> keyIt = mapKeys.iterator();
+
+			while (keyIt.hasNext()) {
+				String key = keyIt.next();
+				Double comp1 = passedMap.get(key);
+				Double comp2 = val;
+
+				if (comp1.equals(comp2)) {
+					keyIt.remove();
+					sortedMap.put(key, val);
+					break;
+				}
+			}
+		}
+		return sortedMap;
+	}
+
+	
+	public static String word_cleaner_regex(String dirty_words){
+		String regex_pattern = "[aA-zZ0-9'éèàùç]+";
+		String words_cleaned = "";
+		Pattern p = Pattern.compile(regex_pattern);
+		Matcher m = p.matcher(dirty_words);
+		while (m.find()) {
+			words_cleaned += m.group() + " ";
+		}
+	return words_cleaned;
+	}
+	
+	
 }
--- MASTER_SHAVADOOP_JAR/src/master_shavadoop/master_compute.java
+++ MASTER_SHAVADOOP_JAR/src/master_shavadoop/master_compute.java
@@ -1,93 +1,85 @@
 package master_shavadoop;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class master_compute {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
 
-		// ================================   Données Inputs   ==========================================
-		String login = "wbirmingham"; // "ghost"; //= "pi";
+		// ================================ Données Inputs ======================================
+		String login = "wbirmingham"; // = "pi"; "ghost"; //
 		String path_cible = "~/workspace/TPTelecom/INF727/"; // Chemin cible
 																// pour l'envoi
 																// du fichier
 																// JAR
-		String file_cible = path_cible + "slave_calcul.jar";
+		String file_cible = "slave_calcul.jar";
 		String ips_source = "liste_ip.txt";
-		String input_file = "Input.txt";
-		// ==============================================================================================
+		String input_file = "forestier_mayotte.txt";	// "Input.txt";		// 
+		String final_file = "output_final.txt";
+		// =======================================================================================
 
-		ArrayList<String> ips_list_ok = new ArrayList<String>();
 		String params = "";
+		ArrayList<String> ips_list_ok = new ArrayList<String>();
+		HashMap<String, Double> list_ip_with_time = new HashMap<String, Double>();
+		HashMap<String, Double> temps_execution_functions = new HashMap<String, Double>();
 		HashMap<String, String> cles_Sx_machines = new HashMap<String, String>();
 		HashMap<String, String> cles_UMx_machines = new HashMap<String, String>();
+		HashMap<String, String> cles_SMx_machines = new HashMap<String, String>();
+		HashMap<String, Integer> cles_RMx_machines = new HashMap<String, Integer>();
 		HashMap<String, ArrayList<String>> cles_UMx_tmp = new HashMap<String, ArrayList<String>>();
-		HashMap<String, ArrayList<String>> cles_UMx = new HashMap<String, ArrayList<String>>();
+		HashMap<String, HashSet<String>> cles_UMx = new HashMap<String, HashSet<String>>();
+		//ArrayList<String> ip_not_selected = new ArrayList<String>();
 		ArrayList<exec_parallele> threads = new ArrayList<exec_parallele>();
+		ArrayList<String> smx_key_return_tmp = new ArrayList<String>();
+		ArrayList<String> params_config = new ArrayList<String>();
 		ArrayList<String> key_return_tmp = new ArrayList<String>();
-		
-		// Vérification des IPs valides
-		String file_logs = functions_tool.check_save_goods_ip(login, ips_source);
 
+		// Vérification des IPs valides
+		String file_logs = ssh_manager.check_save_goods_ip(login, ips_source);
+		
 		// Création de la liste d'IPs valides
 		ips_list_ok = functions_tool.read_file(file_logs);
-
-		// //Selection d'une IP aléatoire
-		// Random rn = new Random();
-		// int ip_index = rn.nextInt(ips_list_ok.size());
-		// int ip_cpt = 0;
-		//
-		// //Execution sur une seule machine
-		// for(String ip_ok_from_list : ips_list_ok){
-		// ip_cpt += 1;
-		// if(ip_cpt == ip_index){
-		// // Envoi du fchier JAR vers la machine distante
-		// compute_distant(ip_ok_from_list.split(" ")[1], login, file_cible,
-		// "");
-		// System.out.println("Fin d'execution");
-		// }
-		// }
-
-		// Execution parallelisée de la simulation de calcul
-		params = " " + "simul";
-		
-		//functions_tool.launch_parallel_exec(ips_list_ok, login, file_cible, params);		// => Probleme de parallelisation
-		
-		for(String ip_ok_from_list : ips_list_ok){
-			exec_parallele thread_ip =  new exec_parallele(ip_ok_from_list.split(" ")[1], login, file_cible, params);
-			thread_ip.start();
-			threads.add(thread_ip);
+		for(String ip_unformated : ips_list_ok){
+			list_ip_with_time.putAll(ssh_manager.extract_ip_from_logs(ip_unformated));
 		}
+		list_ip_with_time = functions_tool.sort_hashmap_by_values(list_ip_with_time);
+	
 		
-		//Attente de la fin d'execution de tous les threads
-		for(exec_parallele thread_ip_launched : threads){
-			thread_ip_launched.join();
-			for(String log_return : thread_ip_launched.getLogs_return_tmp()){
-				key_return_tmp.add(log_return);
-			}
-		}
-		System.out.println("Fin d'execution :" + params);
-		
-		
+		// Simulation de test
+		//functions_tool.simultation_function(list_ip_with_time, login, file_cible, params);
 
-		// 1- Sx Splitting Function Master - création du dictionnaire UMx-machines
-		cles_Sx_machines = functions_tool.splitting_function(ips_list_ok, input_file, login, path_cible);
-		for(String cle_sx : cles_Sx_machines.keySet()){
-			String cle_umx_tmp = "UM" + cles_Sx_machines.get(cle_sx).substring(1, 2);
-			cles_UMx_machines.put(cle_umx_tmp, cle_sx);
-		}
-		System.out.println(cles_UMx_machines);
 		
-		// Execution parallelisée de split_mapping du JAR slave 
-		// extends de la classe Thread à la classe exec_parallele, pour implémenter la function getLogs_return_tmp()
+		// Sx Splitting Function
+		double time_start_1 = java.lang.System.currentTimeMillis();
+		
+		System.out.println("\n*******************************  Splitting Function Launched  *********************************");
+		cles_Sx_machines = functions_tool.splitting_function(list_ip_with_time, login, input_file, path_cible);
+		System.out.println("\ncles_Sx_machines : ");
+		for(String sx : cles_Sx_machines.keySet()){
+			System.out.println("		" + sx + " : " + cles_Sx_machines.get(sx));
+		}
+		System.out.println("\n*************************************************************************************************");		
+		
+		double time_end_1 = java.lang.System.currentTimeMillis();
+		temps_execution_functions.put("Splitting Function", (time_end_1 - time_start_1) / 1000.0);
+		
+		
+		double time_start_2 = java.lang.System.currentTimeMillis();
+		
+		// Execution parallelisée de split_mapping du JAR slave
+		// extends de la classe Thread à la classe exec_parallele, pour
+		// implémenter la function getLogs_return_tmp()
+		System.out.println("\n****************************  Splits Mapping Function Launched  *********************************");
 		for (String el_splitted : cles_Sx_machines.keySet()) {
-			params = " " + "split_mapping " + cles_Sx_machines.get(el_splitted);
-			
-			exec_parallele thread_ip = new exec_parallele(el_splitted, login, file_cible, params);
+			params = " " + "modeSXUMX " + el_splitted;
+			exec_parallele thread_ip = new exec_parallele(cles_Sx_machines.get(el_splitted), login, path_cible, file_cible, params);
 			thread_ip.start();
 			threads.add(thread_ip);
 			System.out.println("Fin d'execution :" + params);
@@ -96,16 +88,23 @@ public class master_compute {
 		// Attente de la fin d'execution
 		for (exec_parallele thread_ip_launched : threads) {
 			thread_ip_launched.join();
-			for(String log_return : thread_ip_launched.getLogs_return_tmp()){
+			for (String log_return : thread_ip_launched.getLogs_return()) {
 				key_return_tmp.add(log_return);
 			}
 		}
 
+		System.out.println("key_return_tmp : " + key_return_tmp);
+		
+		double time_end_2 = java.lang.System.currentTimeMillis();
+		temps_execution_functions.put("Splits Mapping Function", (time_end_2 - time_start_2) / 1000.0);
+		
+		
+		
 		// Création du dictionnaire clés-UMx
 		for (String keys_tmp : key_return_tmp) {
 			ArrayList<String> cles_UMx_sub = new ArrayList<String>();
-			for (String el : keys_tmp.split(" ")) {
-				if (!el.trim().substring(0, 2).equals("UM")) {
+			for (String el : keys_tmp.split(" ")) {		
+				if (!el.trim().substring(0, Math.min(el.length(), 2)).equals("UM")) {
 					cles_UMx_sub.add(el);
 				}
 			}
@@ -113,28 +112,93 @@ public class master_compute {
 				cles_UMx_tmp.put(keys_tmp.split(" ")[0], cles_UMx_sub);
 			}
 		}
-		//System.out.println(cles_UMx_tmp);
+		System.out.println("cles_UMx_tmp : " + cles_UMx_tmp);
+
 		
-		//Création de la clés-UMx
-		for(String key_umx : cles_UMx_tmp.keySet()){
-			for(String key_str_words : cles_UMx_tmp.get(key_umx)){
-				//for(String key_str_words : key_list_words){
-					for(String key_word : key_str_words.split(" ")){
-						if(!cles_UMx.containsKey(key_word)){
-							ArrayList<String> umx_list_tmp = new ArrayList<String>();
-							umx_list_tmp.add(key_umx);
-							cles_UMx.put(key_word, umx_list_tmp);
-						}
-						else{
-							cles_UMx.get(key_word).add(key_umx);
-						}
-							
-					//}
-					
+		
+		// Création de la clés-UMx
+		for (String key_umx : cles_UMx_tmp.keySet()) {
+			for (String key_str_words : cles_UMx_tmp.get(key_umx)) {
+				for (String key_word : key_str_words.split(" ")) {
+					if (!cles_UMx.containsKey(key_word)) {
+						HashSet<String> umx_list_tmp = new HashSet<String>();
+						umx_list_tmp.add(key_umx);
+						cles_UMx.put(key_word, umx_list_tmp);
+					} else {
+						cles_UMx.get(key_word).add(key_umx);
+					}
 				}
 			}
 		}
-		System.out.println(cles_UMx);
+		System.out.println("cles_UMx : " + cles_UMx);
+		System.out.println("Tout est fini");
+
+		double time_start_3 = java.lang.System.currentTimeMillis();
+		
+		
+		// Préparation des paramètres à envoyer aux slaves
+		params = " " + "modeUMXSMX";
+		int index_smx = 0;
+		for (String i_word : cles_UMx.keySet()) {
+			String params_smx_name = "SM" + index_smx;
+			String extend_params = i_word + " " + params_smx_name;
+			for (String i_umx_word : cles_UMx.get(i_word)) {
+				extend_params = extend_params + " " + i_umx_word;
+			}
+			params_config.add(params + " " + extend_params);
+			index_smx += 1;
+		}
+		
+		
+		threads.clear();
+		
+		System.out.println(params_config);
+		int index_select_ip = 0;
+		for (String el_param : params_config) {
+			String ip_word_selected = "";
+		
+			ip_word_selected = list_ip_with_time.keySet().toArray(new String[index_select_ip])[index_select_ip];
+			if(index_select_ip < list_ip_with_time.keySet().size() - 1){
+				index_select_ip += 1;
+			}
+			else{
+				index_select_ip = 0;
+			}
+			exec_parallele thread_ip = new exec_parallele(ip_word_selected, login, path_cible, file_cible, el_param);
+			thread_ip.start();
+			threads.add(thread_ip);
+		}
+
+		// Attente de la fin d'execution de tous les threads
+		for (exec_parallele thread_ip_launched : threads) {
+			thread_ip_launched.join();
+			for (String log_return : thread_ip_launched.getLogs_return()) {
+				smx_key_return_tmp.add(log_return);
+				cles_RMx_machines.put(log_return.split(" ")[0], Integer.parseInt(functions_tool.word_cleaner_regex(log_return.split(" ")[1]).trim()));
+			}
+		}
+		System.out.println("Fin d'execution : " + params);
+		System.out.println("RMx returns : " + cles_RMx_machines);
+		
+		double time_end_3 = java.lang.System.currentTimeMillis();
+		temps_execution_functions.put("Reducing Sorted Maps Function", (time_end_3 - time_start_3) / 1000.0);
+		
+			
+		new File(final_file).delete();
+		for(String final_key_word : cles_RMx_machines.keySet()){
+			String str_final_word = final_key_word + " " + cles_RMx_machines.get(final_key_word);
+			functions_tool.write_file(final_file, str_final_word);
+		}
+		System.out.println("Création du fichier : " + final_file);
+		
+		int tot = 0;
+		for(int el : cles_RMx_machines.values()){
+			tot += el; 
+		}
+		System.out.println("WORD COUNT REPORT : " + tot);
+		System.out.println("Splitting Function : " + temps_execution_functions.get("Splitting Function"));
+		System.out.println("Splits Mapping Function : " + temps_execution_functions.get("Splits Mapping Function"));
+		System.out.println("Reducing Sorted Maps Function : " + temps_execution_functions.get("Reducing Sorted Maps Function"));
 	}
 
 }
